8df85f731760b57d5c078cf39e572fb2b04b5f04
Dustin Stringer <dustin.stringer@spaceforce.mil>
2025-01-15 20:15:16 -0700

    moved schema to models

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Book = require('../models/book');
 
 /**
  * @swagger
@@ -71,92 +72,6 @@ const mongoose = require('mongoose');
  *           type: string
  *           format: date-time
  */
-
-// Book Schema
-const bookSchema = new mongoose.Schema({
-  _id: {
-    type: Object
-  },
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  author: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  genre: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  isbn: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  summary: {
-    type: String,
-    required: true
-  },
-  publisher: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  publicationDate: {
-    type: Date,
-    required: true
-  },
-  pageCount: {
-    type: Number,
-    required: true,
-    min: 1
-  },
-  language: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  coverImageUrl: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 0,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  lowStockThreshold: {
-    type: Number,
-    required: true,
-    min: 1,
-    default: 5
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-// Add index for common queries
-bookSchema.index({ title: 1, author: 1 });
-
-const Book = mongoose.model('Book', bookSchema);
 
 /**
  * @swagger
